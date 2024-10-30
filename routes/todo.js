1e669f75ea4d6788fcadc8aa5e2b941120c862ce
nauvalachid <nauvalachid@gmail.com>
2024-10-30 21:42:14 +0700

    CRUD_RESTAPI

--- routes/todo.js
+++ routes/todo.js
@@ -13,4 +13,30 @@ let todos = [
 // Endpoint untuk mendapatkan data Todos
 router.get('/', (req, res) => { res.json(todos); });
 
+router.post('/', (req, res) => {
+    const newTodo = {
+        id: todos.length + 1,
+        task: req.body.task,
+        completed: false
+    };
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
+});
+router.delete('/:id', (req,res)=>{
+    const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id));
+    if(todoIndex===-1) return res.status(404).json({message: 'Tugas tidak ditemukan'});
+    const deletedTodo = todos.splice(todoIndex, 1)[0];
+    res.status(200).json({message: `Tugas'${deletedTodo.task}'Telah dihapus`});
+});
+router.put('/:id', (req,res)=>{ 
+    const todo = todos.find(t => t.id === parseInt(req.params.id));
+    if (!todo) return res.status(404).json({message: 'Tugas tidak ditemukan'});
+    todo.task = req.body.task || todo.task;
+    res.status(200).json({
+        message: `Tugas dengan ID ${todo.id} telah diperbarui`,
+        updatedTodo:todo
+    });
+});
+
+
 module.exports = router;
